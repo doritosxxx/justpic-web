816ecab6cd6542b4d3ae59afc495ffe05680d1ce
doritosxxx <doritosxxx@users.noreply.github.com>
2021-04-26 21:49:33 +0300

    added dat.gui radio control

---
+++ src/dat-radio.ts
@@ -0,0 +1,50 @@
+import dat from '@jsm/libs/dat.gui.module.js'
+
+type BooleanObject = Record<any, boolean>
+
+// TODO: rewrite as GUIController.
+
+function addRadio(this: dat.GUI, name:string, target:BooleanObject, onChange: (property:string) => void ){
+	const folder = this.addFolder(name)
+
+
+	let activeController: dat.GUIController|null =  null;
+
+	for(let property of Object.keys(target)){
+		const controller = folder.add(target, property).onChange( function(this: dat.GUIController, value: boolean){
+			if(activeController === null)
+				return;
+
+			if(activeController == this){
+				if(value === false)
+					this.setValue(true)
+				return;
+			}
+			if(value === false)
+				return;
+
+			const activeControllerCopy = activeController
+			activeController = this
+			activeControllerCopy.setValue(false)
+
+			onChange(property)
+			
+		})
+		
+		if(target[property] === true)
+			if(activeController === null)
+				activeController = controller;
+			else 
+				target[property] = false;
+	}
+
+	if(activeController === null && Object.keys(target).length !== 0){
+		activeController = folder.__controllers[0]
+		activeController.setValue(true)
+	}
+
+	
+	return folder;
+}
+
+export default addRadio;
